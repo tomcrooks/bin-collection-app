3e54d909155f61d2e477ebd07728db8d79018a81
Tom Crooks <thomas.crooks@pimberly.com>
2023-12-19 12:54:48 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+// Step 5: Import Echo, Air, and Templ
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+    // Step 6: Initialize Echo, Air, and Templ
+    e := echo.New()
+
+    // Initialize Air and Templ as needed
+    // ...
+}
